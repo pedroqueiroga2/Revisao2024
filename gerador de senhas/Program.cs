c3b894c025b54e63235b3df2b0f8ccbd0f3d0f89
pedroqueiroga <pedroqueirogahub@gmail.com>
2025-01-20 10:38:59 -0300

    gerador de senhas 1.0

--- gerador de senhas/Program.cs
+++ gerador de senhas/Program.cs
@@ -1,10 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.PortableExecutable;
+
 Console.WriteLine("informe a quantidade de caracter que você deseja");
 while(true)
 {
     if(int.TryParse(Console.ReadLine(), out int qnt))
     {
+        for(int i=0; i<qnt;i++)
+        {
+        string index;
+        var Random = new Random();
         
+        string[] Caractere = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",  
+"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",  
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",  
+"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?", "`", "~", " "  
+    };
+    var randomIndex = Random.Next(Caractere.Length);
+     index = Caractere[randomIndex];
+     Console.Write(index);
+     
+        }
+
+    break;
     }
     else
     {
