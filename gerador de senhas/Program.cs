72850d37174cd61293f9e4bcb341cb89521ed000
pedroqueiroga <pedroqueirogahub@gmail.com>
2025-01-20 10:07:25 -0300

    adicionando arquivo dotnet

---
+++ gerador de senhas/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("informe a quantidade de caracter que você deseja");
+while(true)
+{
+    if(int.TryParse(Console.ReadLine(), out int qnt))
+    {
+        
+    }
+    else
+    {
+        Console.WriteLine("informe um valor válido");
+    }
+}
